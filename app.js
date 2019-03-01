83379ae3d11ed92e310c4da95fabe9892c4d9008
Jonathan Fernandes <jhfernan@mail.usf.edu>
2019-03-01 00:00:19 -0500

    Now set up to repeat 10,000 times

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const app = new Vue({
 	},
 	methods: {
 		send () {
-			axios.post('https://developer.ebay.com/register_upgrade_keyset', { username: 'getYoShitTogethaBrah' })
-			.then(res => {
-				console.log('Sent')
-			})
-			.catch(err => {
-				console.log('Errored out')
-			})
+			let array = Array(10000).fill().map((_,i) => i + 1)
+			for (var i = 0; i < array.length; i++) {
+				axios.post('https://developer.ebay.com/register_upgrade_keyset', { username: 'getYoShitTogethaBrah' })
+				.then(res => {
+					console.log('Sent')
+				})
+				.catch(err => {
+					console.log('Errored out')
+				})
+			}
 		}
 	}
 })
